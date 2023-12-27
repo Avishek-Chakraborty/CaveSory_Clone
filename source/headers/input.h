a9adab8943498e25381e550736b1c80628a00ef0
Avishek Chakraborty <avishek3141@gmail.com>
2023-12-27 10:27:42 +0530

    2nd comit, "input.h" fixed

--- source/headers/input.h
+++ source/headers/input.h
@@ -1,82 +1,22 @@
-#ifndef LEVEL_H
-#define LEVEL_H
+#ifndef INPUT_H
+#define INPUT_H
 
-#include <string>
-#include <vector>
+#include <SDL2/SDL.h>
+#include <map>
 
-#include "globals.h"
-#include "tile.h"
-#include "rectangle.h"
-#include "slope.h"
-#include "animatedtile.h"
-#include "door.h"
-
-class Graphics;
-class Enemy;
-class Player;
-struct SDL_Texture;
-struct SDL_Rect;
-struct Tileset;
-
-class Level {
+class Input {
 public:
-	Level();
-	Level(std::string mapName, Graphics &graphics);
-	~Level();
-	void update(int elapsedTime, Player &player);
-	void draw(Graphics &graphics);
+	void beginNewFrame();
+	void keyUpEvent(const SDL_Event& event);
+	void keyDownEvent(const SDL_Event& event);
 
-	std::vector<Rectangle> checkTileCollisions(const Rectangle &other);
-	std::vector<Slope> checkSlopeCollisions(const Rectangle &other);
-	std::vector<Door> checkDoorCollisions(const Rectangle &other);
-	std::vector<Enemy*> checkEnemyCollisions(const Rectangle &other);
-
-	const Vector2 getPlayerSpawnPoint() const;
-
+	bool wasKeyPressed(SDL_Scancode key);
+	bool wasKeyReleased(SDL_Scancode key);
+	bool isKeyHeld(SDL_Scancode key);
 private:
-	std::string _mapName;
-	Vector2 _spawnPoint;
-
-	Vector2 _size;
-	Vector2 _tileSize;
-
-	SDL_Texture* _backgroundTexture;
-
-	std::vector<Tile> _tileList;
-	std::vector<Tileset> _tilesets;
-	std::vector<Rectangle> _collisionRects;
-	std::vector<Slope> _slopes;
-
-	std::vector<AnimatedTile> _animatedTileList;
-	std::vector<AnimatedTileInfo> _animatedTileInfos;
-
-	std::vector<Door> _doorList;
-
-	std::vector<Enemy*> _enemies;
-
-	/* void loadMap
-	 * Loads a map
-	 */
-	void loadMap(std::string mapName, Graphics &graphics);
-
-	Vector2 getTilesetPosition(Tileset tls, int gid, int tileWidth, int tileHeight);
-
+	std::map<SDL_Scancode, bool> _heldKeys;
+	std::map<SDL_Scancode, bool> _pressedKeys;
+	std::map<SDL_Scancode, bool> _releasedKeys;
 };
-
-//Tileset structure
-struct Tileset {
-	SDL_Texture* Texture;
-	int FirstGid;
-	Tileset() {
-		this->FirstGid = -1;
-	}
-	Tileset(SDL_Texture* texture, int firstGid) {
-		this->Texture = texture;
-		this->FirstGid = firstGid;
-	}
-};
-
-
-
 
 #endif
